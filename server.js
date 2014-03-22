71aa7abcd03e7d7d4dd3b1d0cf3112404eaa9895
Nobby Knox <nobbyknox@gmail.com>
2014-03-22 17:43:45 +0200

    First stab at schedules.
    I'm making use of the 'cron' package to implement scheduling.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var config = require('./config/' + (process.env.NODE_ENV || 'dev') + '.json');
+var cronJob = require('cron').CronJob;
+var moment = require('moment');
 
 var zwaveHostname = '192.168.10.221';
 var zwavePort = 8083;
@@ -7,8 +9,14 @@ var zwavePort = 8083;
 var express = require('express');
 var app = express();
 
+var EXPRESS_PORT = 3000;
 var ON_VALUE = '255';
 var OFF_VALUE = '0';
+
+
+// ---------------------
+// Express configuration
+// ---------------------
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
@@ -27,9 +35,10 @@ app.use(express.basicAuth(function(user, pass, callback) {
 
 }));
 
-app.get('/hello', function(req, res) {
-    res.send('Hello World!');
-});
+
+// --------------
+// Route Handlers
+// --------------
 
 app.post('/status', function(request, response) {
 
@@ -80,7 +89,7 @@ app.post('/toggle', function(request, response) {
     });
 
     zwaveRequest.on('error', function(e) {
-        console.log('Problem with request: ' + e.message);
+        log('ERROR: Problem with request: ' + e.message);
     });
 
     zwaveRequest.end();
@@ -113,14 +122,33 @@ app.post('/sysinfo', function(request, response) {
         'totalmem': (os.totalmem() / 1024 / 1024) + ' MB',
         'freemem': (os.freemem() / 1024 / 1024) + ' MB'
     };
+
     response.send(info);
     response.end();
 });
 
-var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
+
+// ----------
+// Main Block
+// ----------
+
+var server = app.listen(EXPRESS_PORT, function() {
+
+    log('Listening on port ' + server.address().port);
+    log(server.address());
+
+    startScheduler();
+
 });
 
+
+// --------
+// The rest
+// --------
+
+function log(mesg) {
+    console.log(moment().format('YYYY-MM-DD hh:mm:ss.SSS') + ': ' + mesg);
+}
 
 function setLight(device, instance, state) {
 
@@ -137,9 +165,66 @@ function setLight(device, instance, state) {
     var request = http.request(options);
 
     request.on('error', function(e) {
-        console.log('Problem with request: ' + e.message);
+        log('ERROR: Problem with request: ' + e.message);
     });
 
     request.end();
+
+}
+
+function startScheduler() {
+
+    var schedules = require('./config/schedules.json');
+
+    if (schedules && (schedules.length > 0)) {
+
+        log('Scheduler starting...');
+
+        var cronJob = require('cron').CronJob;
+
+        log('Schedules:');
+
+        schedules.forEach(function(item) {
+            log('  cron: ' + item.cron + ', scene: ' + item.scene);
+
+            new cronJob(item.cron, function() {
+                log('CRON: Triggering scene ' + item.scene);
+//                testScene();
+            }, null, true, null);
+        });
+    } else {
+        log('No schedules found');
+    }
+
+}
+
+function testScene() {
+
+    var options = {
+        hostname: 'localhost',
+        port: EXPRESS_PORT,
+        path: '/toggle',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + new Buffer('test' + ':' + 'test').toString('base64')
+        }
+    };
+
+    var newRequest = http.request(options, function(newResponse) {
+        newResponse.setEncoding('utf8');
+        newResponse.on('data', function(chunk) {
+
+            log('Server said: ' + chunk);
+
+        });
+    });
+
+    newRequest.on('error', function(e) {
+        log('ERROR: Problem with request: ' + e.message);
+    });
+
+    newRequest.write(JSON.stringify({"deviceNum": 5, "instNum": 1}));
+    newRequest.end();
 
 }
