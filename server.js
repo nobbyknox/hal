67645696843be59d01dcb1c6724031c5bd762c6c
Nobby Knox <nobbyknox@gmail.com>
2014-03-23 13:43:01 +0200

    Scheduling now working.

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var http = require('http');
-var config = require('./config/' + (process.env.NODE_ENV || 'dev') + '.json');
 var cronJob = require('cron').CronJob;
 var moment = require('moment');
-
-var zwaveHostname = '192.168.10.221';
-var zwavePort = 8083;
-
 var express = require('express');
-var app = express();
 
-var EXPRESS_PORT = 3000;
+var config = require('./config/' + (process.env.NODE_ENV || 'dev') + '.json');
+var lights = require('./config/lights.json');
+var scenes = require('./config/scenes.json');
+
+var zwaveHostname = config.zwaveHostname;
+var zwavePort = config.zwavePort;
+var expressPort = config.expressPort;
+
 var ON_VALUE = '255';
 var OFF_VALUE = '0';
 
@@ -17,6 +18,8 @@ var OFF_VALUE = '0';
 // ---------------------
 // Express configuration
 // ---------------------
+
+var app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
@@ -67,32 +70,16 @@ app.post('/status', function(request, response) {
 
 app.post('/toggle', function(request, response) {
 
-    var options = {
-        hostname: zwaveHostname,
-        port: zwavePort,
-        path: '/ZWaveAPI/Run/devices[' + request.body.deviceNum + '].instances[' + request.body.instNum + '].commandClasses[0x25].data.level.value',
-        method: 'POST'
-    };
+    // TODO: Must migrate towards light ID away from device/instance IDs
 
-    var zwaveRequest = http.request(options, function(zwaveResponse) {
-        zwaveResponse.setEncoding('utf8');
-        zwaveResponse.on('data', function(chunk) {
+    getLightState(request.body.deviceNum, request.body.instNum, function(currentState) {
+        var newState = (currentState === ON_VALUE ? OFF_VALUE : ON_VALUE);
 
-            var newState = (chunk === ON_VALUE ? OFF_VALUE : ON_VALUE);
+        response.send(newState);
+        response.end();
 
-            response.send(newState);
-            response.end();
-
-            setLight(request.body.deviceNum, request.body.instNum, newState);
-
-        });
+        setLight(request.body.deviceNum, request.body.instNum, newState);
     });
-
-    zwaveRequest.on('error', function(e) {
-        log('ERROR: Problem with request: ' + e.message);
-    });
-
-    zwaveRequest.end();
 
 });
 
@@ -132,19 +119,73 @@ app.post('/sysinfo', function(request, response) {
 // Main Block
 // ----------
 
-var server = app.listen(EXPRESS_PORT, function() {
+var server = app.listen(expressPort, function() {
 
     log('Listening on port ' + server.address().port);
-    log(server.address());
+
+    log('');
+    log('Lights:');
+    lights.forEach(function(light) {
+        log('  id: ' + light.id + ', name: ' + light.name + ', device:instance: ' + light.device + ':' + light.instance);
+    });
+
+    log('');
+    log('Scenes:');
+    scenes.forEach(function(scene) {
+        log('  id: ' + scene.id + ', name: ' + scene.name + ', lights: ' + scene.lights);
+    });
 
     startScheduler();
 
 });
 
 
-// --------
-// The rest
-// --------
+// -------------
+// The Functions
+// -------------
+
+function actionLights(lightIds, action) {
+
+    lightIds.forEach(function(item) {
+        getLight(item, function(light) {
+
+            if ((action === 'on') || (action === 'off')) {
+                setLight(light.device, light.instance, (action === 'on' ? ON_VALUE : OFF_VALUE));
+            } else if (action === 'toggle') {
+                getLightState(light.device, light.instance, function(currentState) {
+                    var newState = (currentState === ON_VALUE ? OFF_VALUE : ON_VALUE);
+                    setLight(light.device, light.instance, newState);
+                });
+            }
+        });
+    });
+}
+
+function getLightState(device, instance, callback) {
+
+    var options = {
+        hostname: zwaveHostname,
+        port: zwavePort,
+        path: '/ZWaveAPI/Run/devices[' + device + '].instances[' + instance + '].commandClasses[0x25].data.level.value',
+        method: 'POST'
+    };
+
+    var request = http.request(options, function(response) {
+
+        response.setEncoding('utf8');
+
+        response.on('data', function(currentState) {
+            callback(currentState);
+        });
+    });
+
+    request.on('error', function(e) {
+        log('ERROR: Problem with request: ' + e.message);
+    });
+
+    request.end();
+
+}
 
 function log(mesg) {
     console.log(moment().format('YYYY-MM-DD hh:mm:ss.SSS') + ': ' + mesg);
@@ -178,53 +219,44 @@ function startScheduler() {
 
     if (schedules && (schedules.length > 0)) {
 
-        log('Scheduler starting...');
-
         var cronJob = require('cron').CronJob;
 
+        log('');
         log('Schedules:');
 
         schedules.forEach(function(item) {
-            log('  cron: ' + item.cron + ', scene: ' + item.scene);
+
+            log('  cron: ' + item.cron + ', sceneId: ' + item.sceneId);
 
             new cronJob(item.cron, function() {
-                log('CRON: Triggering scene ' + item.scene);
-//                testScene();
+                triggerScene(item.sceneId);
             }, null, true, null);
         });
+
+        log('');
     } else {
         log('No schedules found');
     }
 
 }
 
-function testScene() {
+function triggerScene(sceneId) {
+    log('Schedule triggering scene ID ' + sceneId + '...');
+    getScene(sceneId, actionLights);
+}
 
-    var options = {
-        hostname: 'localhost',
-        port: EXPRESS_PORT,
-        path: '/toggle',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Basic ' + new Buffer('test' + ':' + 'test').toString('base64')
+function getLight(id, callback) {
+    lights.forEach(function(item) {
+        if (item.id === id) {
+            callback(item);
         }
-    };
-
-    var newRequest = http.request(options, function(newResponse) {
-        newResponse.setEncoding('utf8');
-        newResponse.on('data', function(chunk) {
-
-            log('Server said: ' + chunk);
-
-        });
     });
+}
 
-    newRequest.on('error', function(e) {
-        log('ERROR: Problem with request: ' + e.message);
+function getScene(sceneId, callback) {
+    scenes.forEach(function(scene) {
+        if (scene.id === sceneId) {
+            callback(scene.lights, scene.action);
+        }
     });
-
-    newRequest.write(JSON.stringify({"deviceNum": 5, "instNum": 1}));
-    newRequest.end();
-
 }
