9bf6403e5c72ddd8e41541aef87319aa93d4778c
Nobby Knox <nobbyknox@gmail.com>
2015-12-31 22:58:56 +0200

    Introduce the authentication service

--- test/data/seeds/users.js
+++ test/data/seeds/users.js
@@ -10,13 +10,13 @@ exports.seed = function (knex, Promise) {
             id: 1,
             email: 'user1@host.com',
             screenName: 'User 1',
-            password: 'abc123'
+            password: '6367c48dd193d56ea7b0baad25b19455e529f5ee'  // abc123
         }),
         knex('users').insert({
             id: 2,
             email: 'user2@host.com',
             screenName: 'User 2',
-            password: 'abc123'
+            password: '6367c48dd193d56ea7b0baad25b19455e529f5ee'  // abc123
         })
     );
 };
