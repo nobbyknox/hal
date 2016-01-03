2993bf81a603565b0b4e3d6ee9e66f247bf695d8
Nobby Knox <nobbyknox@gmail.com>
2016-01-03 11:17:22 +0200

    More light seed data

--- test/data/seeds/lights.js
+++ test/data/seeds/lights.js
@@ -8,48 +8,66 @@ exports.seed = function (knex, Promise) {
         // Inserts seed entries
         knex('lights').insert({
             id: 1,
-            name: 'Lounge',
-            enabled: 1,
-            device: 1,
-            instance: 0,
-            controllerHost: 'localhost',
-            controllerPort: 8080
-        }),
-        knex('lights').insert({
-            id: 2,
             name: 'Front Flood Light',
             enabled: 1,
             device: 2,
             instance: 0,
-            controllerHost: 'localhost',
-            controllerPort: 8080
+            controllerHost: '192.168.10.221',
+            controllerPort: 8083
         }),
         knex('lights').insert({
-            id: 3,
+            id: 2,
             name: 'Side Passage',
             enabled: 1,
             device: 3,
             instance: 0,
-            controllerHost: 'otherhost',
-            controllerPort: 9090
+            controllerHost: '192.168.10.221',
+            controllerPort: 8083
+        }),
+        knex('lights').insert({
+            id: 3,
+            name: 'Side Passage Door',
+            enabled: 1,
+            device: 3,
+            instance: 0,
+            controllerHost: '192.168.10.223',
+            controllerPort: 8083
         }),
         knex('lights').insert({
             id: 4,
-            name: 'Side Entrance',
+            name: 'TV Room',
             enabled: 1,
             device: 4,
             instance: 0,
-            controllerHost: 'otherhost',
-            controllerPort: 9090
+            controllerHost: '192.168.10.223',
+            controllerPort: 8083
         }),
         knex('lights').insert({
             id: 5,
-            name: 'Broken Light',
+            name: 'Art Room',
+            enabled: 0,
+            device: 4,
+            instance: 0,
+            controllerHost: '192.168.10.221',
+            controllerPort: 8083
+        }),
+        knex('lights').insert({
+            id: 6,
+            name: 'Front Lounge',
             enabled: 0,
             device: 5,
+            instance: 1,
+            controllerHost: '192.168.10.221',
+            controllerPort: 8083
+        }),
+        knex('lights').insert({
+            id: 7,
+            name: 'Main Bedroom',
+            enabled: 0,
+            device: 6,
             instance: 0,
-            controllerHost: 'otherhost',
-            controllerPort: 9090
+            controllerHost: '192.168.10.221',
+            controllerPort: 8083
         })
     );
 };
