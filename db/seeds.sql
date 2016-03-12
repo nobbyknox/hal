aa6009499e66720c0643701292e4c820749e9fba
Nobby Knox <nobbyknox@gmail.com>
2016-03-12 15:06:08 +0200

    Add the user seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,31 @@
 BEGIN TRANSACTION;
 
 -- Lights
-insert into "lights" (id, name, enabled, device, instance, controllerHost, controllerPort) values (1, 'Front Spotlight', 1, 1, 0, 'host1', 8083);
-insert into "lights" (id, name, enabled, device, instance, controllerHost, controllerPort) values (2, 'Front Lounge', 1, 2, 0, 'host1', 8083);
-insert into "lights" (id, name, enabled, device, instance, controllerHost, controllerPort) values (3, 'Main Garage', 1, 3, 0, 'host1', 8083);
-insert into "lights" (id, name, enabled, device, instance, controllerHost, controllerPort) values (4, 'Side Passage', 1, 3, 0, 'host1', 8083);
+INSERT into "lights" (id, name, enabled, device, instance, controllerHost, controllerPort) VALUES (1, 'Front Spotlight', 1, 1, 0, 'host1', 8083);
+INSERT into "lights" (id, name, enabled, device, instance, controllerHost, controllerPort) VALUES (2, 'Front Lounge', 1, 2, 0, 'host1', 8083);
+INSERT into "lights" (id, name, enabled, device, instance, controllerHost, controllerPort) VALUES (3, 'Main Garage', 1, 3, 0, 'host1', 8083);
+INSERT into "lights" (id, name, enabled, device, instance, controllerHost, controllerPort) VALUES (4, 'Side Passage', 1, 3, 0, 'host1', 8083);
 
 -- Scenes
-insert into "scenes" (id, name, description, visible, enabled, buttonClass, iconClass, action) values (1, 'All On', 'Turns everything on', 1, 1, 'button', 'icon', 'on');
-insert into "scenes" (id, name, description, visible, enabled, buttonClass, iconClass, action) values (2, 'All Off', 'Turns everything off', 1, 1, 'button', 'icon', 'off');
-insert into "scenes" (id, name, description, visible, enabled, buttonClass, iconClass, action) values (3, 'Arrive At Night', 'Turns on a few lights when we arrive at night', 1, 1, 'button', 'icon', 'on');
+INSERT into "scenes" (id, name, description, visible, enabled, buttonClass, iconClass, action) VALUES (1, 'All On', 'Turns everything on', 1, 1, 'button', 'icon', 'on');
+INSERT into "scenes" (id, name, description, visible, enabled, buttonClass, iconClass, action) VALUES (2, 'All Off', 'Turns everything off', 1, 1, 'button', 'icon', 'off');
+INSERT into "scenes" (id, name, description, visible, enabled, buttonClass, iconClass, action) VALUES (3, 'Arrive At Night', 'Turns on a few lights when we arrive at night', 1, 1, 'button', 'icon', 'on');
 
 -- Lights of scenes
-insert into "scenes_lights" (id, scene_id, light_id) values (1, 1, 1);
-insert into "scenes_lights" (id, scene_id, light_id) values (2, 1, 2);
-insert into "scenes_lights" (id, scene_id, light_id) values (3, 1, 3);
-insert into "scenes_lights" (id, scene_id, light_id) values (4, 1, 4);
+INSERT into "scenes_lights" (id, scene_id, light_id) VALUES (1, 1, 1);
+INSERT into "scenes_lights" (id, scene_id, light_id) VALUES (2, 1, 2);
+INSERT into "scenes_lights" (id, scene_id, light_id) VALUES (3, 1, 3);
+INSERT into "scenes_lights" (id, scene_id, light_id) VALUES (4, 1, 4);
 
-insert into "scenes_lights" (id, scene_id, light_id) values (5, 2, 1);
-insert into "scenes_lights" (id, scene_id, light_id) values (6, 2, 2);
-insert into "scenes_lights" (id, scene_id, light_id) values (7, 2, 3);
-insert into "scenes_lights" (id, scene_id, light_id) values (8, 2, 4);
+INSERT into "scenes_lights" (id, scene_id, light_id) VALUES (5, 2, 1);
+INSERT into "scenes_lights" (id, scene_id, light_id) VALUES (6, 2, 2);
+INSERT into "scenes_lights" (id, scene_id, light_id) VALUES (7, 2, 3);
+INSERT into "scenes_lights" (id, scene_id, light_id) VALUES (8, 2, 4);
 
-insert into "scenes_lights" (id, scene_id, light_id) values (9, 3, 1);
+INSERT into "scenes_lights" (id, scene_id, light_id) VALUES (9, 3, 1);
+
+-- Users
+INSERT into "users" (id, email, screenName, password, enabled) VALUES (1, 'test1@host.com', 'Unicode', 'abc', 1);
+INSERT into "users" (id, email, screenName, password, enabled) VALUES (2, 'test2@host.com', 'ASCII', 'abc', 1);
 
 COMMIT;
