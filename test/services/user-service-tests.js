a6669ee28b8f632bfc20d623f07ce2a55bcfbed4
Nobby Knox <nobbyknox@gmail.com>
2016-03-25 11:59:50 +0200

    Try the new raw function of the data store
    I'll find a proper home for it later.

--- test/services/user-service-tests.js
+++ test/services/user-service-tests.js
@@ -1,6 +1,12 @@
 'use strict';
 
 let service = require('../../lib/services/user-service.js');
+let dataStore = require('../../lib/dao/data-store.js');
+let models = require('../../lib/dao/models.js');
+
+let helper = require('../../lib/helper.js');
+let config = helper.getConfig();
+let log = require('bunyan').createLogger(config.loggerOptions);
 
 var assert = require('assert');
 var path = require('path');
@@ -35,4 +41,17 @@ describe(path.basename(__filename), function() {
             });
         });
     });
+
+    describe('Raw test', function() {
+
+        it('should list all users with rawest of queries', function(done) {
+            dataStore.raw('select email, screenName from users where id = ?', ['dbc23146-7467-4656-a843-5523dfa016cf'], (err, users) => {
+                log.debug(err);
+                log.debug(users);
+                done();
+            });
+        });
+
+    });
+
 });
