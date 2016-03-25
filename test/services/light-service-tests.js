f49624fd03949357d6e648d4d69f54cdf46902ba
Nobby Knox <nobbyknox@gmail.com>
2016-03-25 11:50:15 +0200

    Manual unit test database creation and seeding
    The in-memory SQLite database proved to be a real pain in the ass
    when using more than 1 database connections. The solution was
    simple: use a disk file.
    
    But, that's not all that happened. The migration scripts and seed
    data that was used to create and seed the database through Knex's
    facilities were discarded for a manual load process, and here is
    the clincher, with the same schema file that is used to create the
    development databases and eventually, production database. We're
    also loading the seed data used for development.
    
    This all means:
    
    - The unit test database problem with multiple connection has been
      resolved.
    - We use the same schema and seed data for unit testing and
      development.

--- test/services/light-service-tests.js
+++ test/services/light-service-tests.js
@@ -4,43 +4,48 @@ let service = require('../../lib/services/light-service.js');
 
 var assert = require('assert');
 var path = require('path');
+var util = require('util');
 
 describe(path.basename(__filename), function() {
-
-    before(function(done) {
-        var timer = setInterval(function() {
-            if (global.unittestDataSeeded) {
-                clearInterval(timer);
-                done();
-            }
-        }, 50);
-    });
 
     describe('Retrieval test', function() {
 
         it('should find with ID', (done) => {
             service.find('bd59cf73-8fbe-4505-a8d0-42018cab3820', (err, light) => {
-                assert(!err);
-                assert(light);
-                assert(light.id === 'bd59cf73-8fbe-4505-a8d0-42018cab3820');
-                done();
+                if (err) return done(err);
+
+                try {
+                    assert(light);
+                    assert.equal(light.id, 'bd59cf73-8fbe-4505-a8d0-42018cab3820');
+                    done();
+                } catch (err) {
+                    done(err);
+                }
             });
         });
 
         it('should get all enabled lights', (done) => {
             service.get(1, (err, lights) => {
-                assert(!err);
-                assert(lights);
-                done();
+                if (err) return done(err);
+
+                try {
+                    assert(lights, 'Should have found some lights');
+                    assert.equal(lights.length, 4, util.format('Expected to get 4 lights, but got %d instead.', lights.length));
+                    done();
+                } catch (err) {
+                    done(err);
+                }
             });
         });
 
-        it('should get all disabled lights', (done) => {
-            service.get(0, (err, lights) => {
-                assert(!err);
-                assert(lights);
-                done();
-            });
-        });
+        // TODO: The get function will change. This test will then be
+        // updated accordingly.
+        // it('should get all disabled lights', (done) => {
+        //     service.get(0, (err, lights) => {
+        //         assert(!err);
+        //         assert(lights);
+        //         done();
+        //     });
+        // });
     });
 });
